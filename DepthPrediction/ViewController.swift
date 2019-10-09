ce42d385554ac6f9d067e2294d9e62eeaf594c66
Koki Ibukuro <koki.ibukuro@gmail.com>
2019-10-09 22:38:20 +0200

    Export grayscale png

--- DepthPrediction/ViewController.swift
+++ DepthPrediction/ViewController.swift
@@ -9,7 +9,7 @@
 import Cocoa
 import CoreML
 import Vision
-
+import Accelerate
 
 class ViewController: NSViewController {
 
@@ -49,18 +49,86 @@ class ViewController: NSViewController {
         try? handler.perform([request])
     }
     
+    @IBAction func onExport(_ sender: Any) {
+        print("on export")
+        
+        let panel = NSSavePanel()
+        panel.nameFieldStringValue = "depth.png"
+        panel.begin { (result) in
+            if result.rawValue == NSApplication.ModalResponse.OK.rawValue {
+                guard let image = self.dstImageView.image else {
+                    return
+                }
+                guard let url = panel.url else {
+                    return
+                }
+                self.saveToPng(image: image, url: url)
+            }
+        }
+    }
+    
+    private func saveToPng(image:NSImage, url: URL) {
+        // TO png
+        // https://stackoverflow.com/questions/17507170/how-to-save-png-file-from-nsimage-retina-issues
+        guard let image = self.dstImageView.image?.cgImage(forProposedRect: nil, context: nil, hints: nil) else {
+            return
+        }
+        let newRep = NSBitmapImageRep.init(cgImage: image)
+        newRep.size = NSSize(width: image.width, height: image.height)
+        guard let data = newRep.representation(using: .png, properties: [:]) else {
+            return
+        }
+        try? data.write(to: url)
+    }
     
     private func onVisionRequestComplete(request: VNRequest, error: Error?) {
         guard let observations = request.results as? [VNCoreMLFeatureValueObservation] else {
             return
         }
-        guard let heatmap = observations.first?.featureValue.multiArrayValue else {
+        guard let depth = observations.first?.featureValue.multiArrayValue else {
             return
         }
         
-        print(heatmap)
-        print("convert to grayscale image")
+        print(depth)
+        
+        // Find min max
+        let minmax = depth.minmaxValue()
+        print("min max")
+        print(minmax)
+        
+        // Invert min max
+        guard let image = depth.cgImage(min: minmax.1, max: minmax.0) else {
+            return
+        }
+        
+        let size = self.srcImageView.image!.size
+        self.dstImageView.image = NSImage.init(cgImage: image, size: size)
     }
-    
 }
 
+extension MLMultiArray {
+    public func minmaxValue() -> (Double, Double) {
+        if(self.dataType != .double) {
+            fatalError("Non supported")
+        }
+        
+        var minValue = Double.greatestFiniteMagnitude
+        var maxValue = -Double.greatestFiniteMagnitude
+        
+        // Fast version
+//        var index: vDSP_Length = 0
+//        let ptr = UnsafeMutablePointer<Double>(OpaquePointer(self.dataPointer))
+//        vDSP_maxviD(ptr, vDSP_Stride(-1), &maxValue, &index, vDSP_Length(self.count))
+//        vDSP_minviD(ptr, vDSP_Stride(-1), &minValue, &index, vDSP_Length(self.count))
+        
+        
+        // Slow version
+        for i in 0..<self.count {
+            let n = self[i].doubleValue
+            minValue = min(n, minValue)
+            maxValue = max(n, maxValue)
+        }
+        
+        return (minValue, maxValue)
+    }
+}
