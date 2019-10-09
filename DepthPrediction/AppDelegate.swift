f6007fa013510a18790cc844a201f4a2d99f46ed
Koki Ibukuro <koki.ibukuro@gmail.com>
2019-10-09 15:06:55 +0200

    First commt

---
+++ DepthPrediction/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DepthPrediction
+//
+//  Created by Koki Ibukuro on 2019/10/09.
+//  Copyright © 2019 Koki Ibukuro. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DepthPrediction/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DepthPrediction
+//
+//  Created by Koki Ibukuro on 2019/10/09.
+//  Copyright © 2019 Koki Ibukuro. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
